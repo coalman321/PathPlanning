b6ba073b2d1fbc35ff67ff3c4814c2b115431b58
coalman321 <coalman321@users.noreply.github.com>
2018-06-24 12:56:22 -0400

    eh

--- Astar/src/MainV2.java
+++ Astar/src/MainV2.java
@@ -9,7 +9,7 @@ public class MainV2 {
 
     public static void main(String[] args){
         System.out.println("creating map");
-        MapV2 inst = new MapV2(new File("C:\\Users\\Lynn's\\Desktop\\test.txt"), 100,100, new MapRegion(0,0,100,100));
+        MapV2 inst = new MapV2(new File("C:\\Users\\Lynn's\\Desktop\\test.txt"), 1000,1000, new MapRegion(0,0,1,1));
         System.out.println("map created");
         //System.out.println("adjacent to " + inst.getNode(2,2) + " " + inst.adjacentTo(inst.getNode(2,2)));
         //inst.loadRegion(new MapRegion(20,20,20,20));
@@ -24,6 +24,7 @@ public class MainV2 {
         Node end = new Node(input.nextInt(),input.nextInt());
         System.out.println("path from " + start + " to " + end);
         System.out.println(inst.getPathBetween(start, end, 10));
+        input.close();
 
     }
 
