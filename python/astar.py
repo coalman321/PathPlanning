19c702598f9426747329bd6a862f6f53474dac0a
coalman321 <coalman321@users.noreply.github.com>
2018-09-04 21:29:57 -0400

    ok

---
+++ python/astar.py
@@ -0,0 +1,11 @@
+import numpy as np
+import enum
+
+class status(enum):
+    UNKNOWN = 0
+    TRAVERSED = 1
+    PASSSABLE = 2
+    IMPASSIBLE = 3
+
+class
+
---
+++ python/test.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr = np.zeros((10, 10), np.int8)
+print(arr)
